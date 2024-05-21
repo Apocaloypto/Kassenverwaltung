e450f937b16426f7af2d905c21fc56fe1a3b53ba
Apocaloypto <51018969+Apocaloypto@users.noreply.github.com>
2024-05-21 20:20:16 +0200

    Datei neu/oeffnen

--- Kassenverwaltung/Form1.Designer.cs
+++ Kassenverwaltung/Form1.Designer.cs
@@ -29,20 +29,65 @@
       private void InitializeComponent()
       {
          System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+         menuStrip1 = new MenuStrip();
+         dateiToolStripMenuItem = new ToolStripMenuItem();
+         neuToolStripMenuItem = new ToolStripMenuItem();
+         öffnenToolStripMenuItem = new ToolStripMenuItem();
+         menuStrip1.SuspendLayout();
          SuspendLayout();
+         // 
+         // menuStrip1
+         // 
+         menuStrip1.Items.AddRange(new ToolStripItem[] { dateiToolStripMenuItem });
+         menuStrip1.Location = new Point(0, 0);
+         menuStrip1.Name = "menuStrip1";
+         menuStrip1.Size = new Size(800, 24);
+         menuStrip1.TabIndex = 0;
+         menuStrip1.Text = "menuStrip1";
+         // 
+         // dateiToolStripMenuItem
+         // 
+         dateiToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { neuToolStripMenuItem, öffnenToolStripMenuItem });
+         dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+         dateiToolStripMenuItem.Size = new Size(46, 20);
+         dateiToolStripMenuItem.Text = "Datei";
+         // 
+         // neuToolStripMenuItem
+         // 
+         neuToolStripMenuItem.Name = "neuToolStripMenuItem";
+         neuToolStripMenuItem.Size = new Size(180, 22);
+         neuToolStripMenuItem.Text = "Neu";
+         neuToolStripMenuItem.Click += OnMenuStrip_Neu;
+         // 
+         // öffnenToolStripMenuItem
+         // 
+         öffnenToolStripMenuItem.Name = "öffnenToolStripMenuItem";
+         öffnenToolStripMenuItem.Size = new Size(180, 22);
+         öffnenToolStripMenuItem.Text = "Öffnen";
+         öffnenToolStripMenuItem.Click += OnMenuStrip_Oeffnen;
          // 
          // Form1
          // 
          AutoScaleDimensions = new SizeF(7F, 15F);
          AutoScaleMode = AutoScaleMode.Font;
          ClientSize = new Size(800, 450);
+         Controls.Add(menuStrip1);
          Icon = (Icon)resources.GetObject("$this.Icon");
+         MainMenuStrip = menuStrip1;
          Name = "Form1";
          StartPosition = FormStartPosition.CenterScreen;
          Text = "Kassenverwaltung";
+         menuStrip1.ResumeLayout(false);
+         menuStrip1.PerformLayout();
          ResumeLayout(false);
+         PerformLayout();
       }
 
       #endregion
+
+      private MenuStrip menuStrip1;
+      private ToolStripMenuItem dateiToolStripMenuItem;
+      private ToolStripMenuItem neuToolStripMenuItem;
+      private ToolStripMenuItem öffnenToolStripMenuItem;
    }
 }
--- Kassenverwaltung/Form1.cs
+++ Kassenverwaltung/Form1.cs
@@ -1,10 +1,51 @@
+using Kassenverwaltung.Database;
+
 namespace Kassenverwaltung
 {
    public partial class Form1 : Form
    {
+      private const string FILE_FILTER = "Kassenverwaltungsdatei (*.kdb)|*.kdb";
+
+      private KVDatabase? _database;
+      private string DefaultWindowTitle { get; }
+
       public Form1()
       {
-         InitializeComponent();
+         InitializeComponent(); 
+         DefaultWindowTitle = Text;
+      }
+
+      private void OpenDatabase(string filename)
+      {
+         _database = new KVDatabase($"Data Source={filename}");
+         Text = $"{DefaultWindowTitle} - {filename}";
+      }
+
+      private void OnMenuStrip_Neu(object sender, EventArgs e)
+      {
+         using (var sfd = new SaveFileDialog())
+         {
+            sfd.Filter = FILE_FILTER;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+               OpenDatabase(sfd.FileName);
+               _database!.EnsureExists();
+            }
+         }
+      }
+
+      private void OnMenuStrip_Oeffnen(object sender, EventArgs e)
+      {
+         using (var ofd = new OpenFileDialog())
+         {
+            ofd.Filter = FILE_FILTER;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+               OpenDatabase(ofd.FileName);
+            }
+         }
       }
    }
 }
