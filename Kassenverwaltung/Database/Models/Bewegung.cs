af1ee52c75ee15ee487c760280217c9362f7feaf
Apocaloypto <51018969+Apocaloypto@users.noreply.github.com>
2024-05-22 19:01:29 +0200

    iKategorie nullable

--- Kassenverwaltung/Database/Models/Bewegung.cs
+++ Kassenverwaltung/Database/Models/Bewegung.cs
@@ -4,7 +4,7 @@
    {
       public int Id { get; set; }
       public int iKonto { get; set; }
-      public int iKategorie { get; set; }
+      public int? iKategorie { get; set; }
       public int? iBewegung { get; set; } // Bei Umbuchungen: Schwesterbewegung
       public DateTime Datum { get; set; }
       public decimal Betrag { get; set; }
